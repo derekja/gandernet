b876d659c2c6d96582115f61d15596a9430fd9c6
Derek jacoby <derekja@gmail.com>
2025-01-12 16:41:35 -0800

    update member list, add some website detail

--- gander-network/src/pages/About.js
+++ gander-network/src/pages/About.js
@@ -5,13 +5,15 @@ function About() {
     <div className="about">
       <h1>About the Gander Network</h1>
       <p>
-        The Gander Network is a community of professionals dedicated to
-        advancing innovation and collaboration in our field. Our goals are:
+        The Geospatial ANalytics for Defense Engaged Research (Gander) Network is a community of individuals from academia, non-profit, for-profit, and government
+        who are dedicated to
+        advancing innovation and collaboration in the use of Artificial Intelligence and geospatial analytics. Our goals are:
       </p>
       <ul>
-        <li>Foster meaningful connections between members</li>
+        <li>Foster meaningful connections between members through events and activities</li>
         <li>Promote knowledge sharing and professional development</li>
-        <li>Support innovative projects and initiatives</li>
+        <li>Support innovative projects and initiatives that benefit Canada</li>
+        <li>Support training opportunities for students</li>
       </ul>
     </div>
   );
--- gander-network/src/pages/Website.js
+++ gander-network/src/pages/Website.js
@@ -5,18 +5,20 @@ function Website() {
     <div className="website">
       <h1>Website Plans</h1>
       <div className="website-plans">
-        <p>Our website will feature a modern, responsive design with:</p>
+        <p>In Dr. Coady's lab we have been using a videoconferencing platform that allows for spatial collaborations.
+          Each individual has a video bubble that they can move around on the canvas to hear only those participants that they are next to.
+          This has allowed for images and documents to appear on the website surface, but we have ideas for how to incoporate maps and satellite imagery to create an interaction space specifically tailored to interactive briefings on geospatial topics.
+          This development work has started on top of the Agora videoconferencing platform.</p>
         <ul>
-          <li>Interactive elements</li>
-          <li>Real-time updates</li>
-          <li>Secure user authentication</li>
-          <li>Mobile optimization</li>
+          <li>Secure videoconferencing</li>
+          <li>Integrated mapping tools for annotations and analytics</li>
+          <li>Persistent spaces and transient event spaces</li>
+          <li>Mobile optimized views</li>
         </ul>
       </div>
       
       <div className="image-placeholders">
-        <img src="https://via.placeholder.com/600x400" alt="Placeholder 1" />
-        <img src="https://via.placeholder.com/600x400" alt="Placeholder 2" />
+        <img src="/photos/spatialchat.png" alt="Spatial Chat" width="400" />
       </div>
     </div>
   );
